496b59d3f4e77b2e23ffaca3c08e7f304c0b56db
Vladimir Shaleev <vladimirshaleev@gmail.com>
2016-11-29 18:29:08 +0300

    Add vglyph.c
    Add source file vglyph.c

---
+++ sources/vglyph.c
@@ -0,0 +1,40 @@
+/* vglyph - library for visualize glyphs
+ *
+ * File: vglyph.c
+ * Copyright (C) 2016 Shaleev Vladimir
+ */
+
+#include "vglyph-api.h"
+#include "vglyph-types.h"
+
+int
+vglyph_version(void)
+{
+    return VGLYPH_VERSION;
+}
+
+const char*
+vglyph_version_string(void)
+{
+    return VGLYPH_VERSION_STRING;
+}
+
+vglyph_bool_t
+vglyph_is_version_compatible(int compile_version)
+{
+    return (VGLYPH_VERSION >> 8) >= (compile_version >> 8) ? TRUE : FALSE;
+}
+
+const char*
+vglyph_state_to_string(vglyph_state_t state)
+{
+    switch (state)
+    {
+        case VGLYPH_STATE_SUCCESS:
+            return "no error has occurred";
+
+        case VGLYPH_STATE_OUT_OF_MEMORY:
+            return "out of memory";
+    }
+    return "<unknown error state>";
+}
