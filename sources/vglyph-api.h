eb38ce00ad713be5f602a76b36f10247764f73a8
Vladimir Shaleev <vladimirshaleev@gmail.com>
2016-11-29 18:53:53 +0300

    Add files vglyph-api.h and vglyph-type.h
    Add headers files:
    - vglyph-api.h
    - vglyph-type.h

---
+++ sources/vglyph-api.h
@@ -0,0 +1,30 @@
+﻿/* vglyph - library for visualize glyphs
+ *
+ * File: vglyph-api.h
+ * Copyright (C) 2016 Shaleev Vladimir
+ */
+
+#ifndef VGLYPH_API_H
+#define VGLYPH_API_H
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#if defined (_MSC_VER) && !defined (VGLYPH_STATIC_BUILD)
+# define vglyph_public __declspec(dllexport)
+#endif
+
+#if defined (_MSC_VER) && !defined (__cplusplus)
+# undef inline
+# define inline __inline
+#endif
+
+#ifdef __STRICT_ANSI__
+# undef inline
+# define inline __inline__
+#endif
+
+#include "vglyph.h"
+
+#endif
---
+++ sources/vglyph-types.h
@@ -0,0 +1,23 @@
+﻿/* vglyph - library for visualize glyphs
+ *
+ * File: vglyph-types.h
+ * Copyright (C) 2016 Shaleev Vladimir
+ */
+
+#ifndef VGLYPH_TYPES_H
+#define VGLYPH_TYPES_H
+
+#ifndef FALSE
+# define FALSE 0
+#endif
+
+#ifndef TRUE
+# define TRUE 1
+#endif
+
+typedef intptr_t  vglyph_sint_t;
+typedef uintptr_t vglyph_uint_t;
+typedef int8_t    vglyph_sint8_t;
+typedef uint8_t   vglyph_uint8_t;
+
+#endif
