6e09b8de4b442e10bd952671fef3687708b2f37f
Vladimir Shaleev <vladimirshaleev@gmail.com>
2016-11-29 23:31:58 +0300

    Add project vglyph-test
    Add new project vglyp-test

---
+++ test/test.c
@@ -0,0 +1,39 @@
+/* vglyph-test - test library for visualize glyphs
+ *
+ * File: test.c
+ * Copyright (C) 2016 Shaleev Vladimir
+ */
+
+#include "vglyph.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_glyph_info()
+{
+#ifdef VGLYPH_STATIC_BUILD
+    printf("=================================================\n");
+    printf("test vglyph-static - library for visualize glyphs\n");
+    printf("=================================================\n\n");
+#else
+    printf("==========================================\n");
+    printf("test vglyph - library for visualize glyphs\n");
+    printf("==========================================\n\n");
+#endif
+
+    printf("compile vglyph version: %s (%d)\n", VGLYPH_VERSION_STRING, VGLYPH_VERSION);
+    printf("runtime vglyph version: %s (%d)\n\n", vglyph_version_string(), vglyph_version());
+
+    if (!vglyph_is_version_compatible(VGLYPH_VERSION))
+    {
+        printf("compile and runtime versions not compatible\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(void)
+{
+    show_glyph_info();
+
+    return EXIT_SUCCESS;
+}
