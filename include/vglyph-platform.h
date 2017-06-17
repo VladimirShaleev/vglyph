8635d506ddfa43e32c5db956f21c5feca71dec2a
Vladimir Shaleev <vladimirshaleev@gmail.com>
2017-06-17 19:41:01 +0300

    Update vglyph-platform.h
    Change in vglyph-platform.h:
    - Update platform specific code (Windows platform) for _MSC_VER less 1600
    - Ability disable the stdint.h (macro VGLYPH_NO_STDINT_H)

--- include/vglyph-platform.h
+++ include/vglyph-platform.h
@@ -7,8 +7,6 @@
 #ifndef VGLYPH_PLATFORM_H
 #define VGLYPH_PLATFORM_H
 
-#include <stdint.h>
-
 #ifdef __cplusplus
 # define VGLYPH_BEGIN extern "C" {
 # define VGLYPH_END   }
@@ -22,6 +20,19 @@
 #  define vglyph_public __declspec(dllimport)
 # else
 #  define vglyph_public
+# endif
+#endif
+
+#ifndef VGLYPH_NO_STDINT_H
+# if defined (_MSC_VER) && (_MSC_VER < 1600)
+   typedef signed   __int8  int8_t;
+   typedef unsigned __int8  uint8_t;
+   typedef signed   __int16 int16_t;
+   typedef unsigned __int16 uint16_t;
+   typedef signed   __int32 int32_t;
+   typedef unsigned __int32 uint32_t;
+# else
+#  include <stdint.h>
 # endif
 #endif
 
