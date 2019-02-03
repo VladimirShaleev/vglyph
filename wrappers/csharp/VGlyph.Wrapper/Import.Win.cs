3c51ca20070fb9d2d53127a5bfe606d9255c3063
Vladimir Shaleev <vladimirshaleev@gmail.com>
2019-02-03 22:40:39 +0300

    Add Import.Win to VGlyph Wrapper project

---
+++ wrappers/csharp/VGlyph.Wrapper/Import.Win.cs
@@ -0,0 +1,25 @@
+﻿#if WIN
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace VGlyph
+{
+    internal static partial class Import
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string path);
+
+        static Import()
+        {
+            var directory = Path.GetDirectoryName(typeof(Import).Assembly.Location);
+            var folder = Environment.Is64BitProcess ? "x64" : "x86";
+            var libraryPath = Path.Combine(directory, folder, "vglyph.dll");
+
+            LoadLibrary(libraryPath);
+        }
+    }
+}
+
+#endif
--- wrappers/csharp/VGlyph.Wrapper/Import.cs
+++ wrappers/csharp/VGlyph.Wrapper/Import.cs
@@ -1,34 +1,17 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace VGlyph
 {
-    internal static class Import
+    internal static partial class Import
     {
         [DllImport(_library, EntryPoint = "vglyph_version")]
         public static extern uint Version();
 
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr LoadLibrary(string path);
-
-        static Import()
-        {
-            var directory = Path.GetDirectoryName(typeof(Import).Assembly.Location);
-            var folder = Environment.Is64BitProcess ? "x64" : "x86";
-            var libraryPath = Path.Combine(directory, folder, "vglyph.dll");
-
-            LoadLibrary(libraryPath);
-        }
-
         private const string _library =
-#if NET45
+#if WIN
             "vglyph.dll";
-#elif ANDROID
-            "libvglyph.so";
 #else
             " ";
 #endif
     }
-
 }
