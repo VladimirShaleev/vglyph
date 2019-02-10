94615496fc0b0c43bbf50de636adc783e7cd9280
Vladimir Shaleev <vladimirshaleev@gmail.com>
2019-02-10 13:03:49 +0300

    Add docs for VGlyph.Wrapper project

--- wrappers/csharp/VGlyph.Wrapper/Coordinate.cs
+++ wrappers/csharp/VGlyph.Wrapper/Coordinate.cs
@@ -1,8 +1,19 @@
 ﻿namespace VGlyph
 {
+    /// <summary>
+    /// Indicates how to shift the coordinate
+    /// </summary>
     public enum Coordinate
     {
+        /// <summary>
+        /// Absolute coordinate
+        /// </summary>
         Absolute,
+
+        /// <summary>
+        /// For the relative versions of the commands, all coordinate 
+        /// values are relative from previous coordinate.
+        /// </summary>
         Relative
     }
 }
--- wrappers/csharp/VGlyph.Wrapper/Point.cs
+++ wrappers/csharp/VGlyph.Wrapper/Point.cs
@@ -2,7 +2,47 @@
 
 namespace VGlyph
 {
+    /// <summary>
+    /// Coordinate
+    /// </summary>
+    [Serializable]
     public struct Point
+    {
+        [Explicit] private float _x;
+        [Explicit] private float _y;
+
+        /// <summary>
+        /// Coordinate X
+        /// </summary>
+        public float X
+        {
+            get => _x;
+            set => _x = value;
+        }
+
+        /// <summary>
+        /// Coordinate Y
+        /// </summary>
+        public float Y
+        {
+            get => _y;
+            set => _y = value;
+        }
+
+        /// <summary>
+        /// Initialize <see cref="Point"/>
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = false)]
+    internal class ExplicitAttribute : Attribute
     {
     }
 }
